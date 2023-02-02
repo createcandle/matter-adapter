295e006150495bfd2bec4eb5bed3bc874141fb1e
flatsiedatsie <unfold@gmail.com>
2023-02-02 15:31:29 +0100

    Update package.sh

--- package.sh
+++ package.sh
@@ -21,6 +21,7 @@ fi
 
 
 echo "installing apt packages"
+sudo apt-get update
 sudo apt-get install libcairo2-dev libjpeg-dev libpango1.0-dev libgif-dev build-essential g++
 # Not sure is libjpeg-dev is the correct one
 

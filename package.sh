c43609cbbc7a34856ab5f36c0466ae64df8c47e6
flatsiedatsie <unfold@gmail.com>
2023-02-02 16:20:42 +0100

    Update package.sh

--- package.sh
+++ package.sh
@@ -30,7 +30,12 @@ sudo apt-get install libcairo2-dev libjpeg-dev libpango1.0-dev libgif-dev build-
 echo "creating package"
 mkdir -p lib package
 
-python3 -m pip install --upgrade pip
+PIPPY="pip3"
+pip3.11 --version && PIPPY="pip3.11 -m pip"
+echo "PIP STRING: $PIPPY"
+
+# Is upgrading pip needed?
+"$PIPPY" install --upgrade pip
 
 # Pull down Python dependencies
 
@@ -49,11 +54,18 @@ python3 -m pip install --upgrade pip
 #pip3 install aiorun -t lib --no-binary :all: --prefix ""
 #pip3 install python-matter-server[server] -t lib  --prefix ""
 
-if [ -e /usr/local/bin/pip3.11 ]; then
-  /usr/local/bin/pip3.11 -m pip install -r requirements.txt -t lib --no-cache-dir --no-binary  :all: --prefix ""
-else
-  pip3 install -r requirements.txt -t lib -no-cache-dir  --no-binary  :all: --prefix ""
-fi
+wget -c https://github.com/home-assistant-libs/chip-wheels/releases/download/2023.1.0/home_assistant_chip_core-2023.1.0-cp37-abi3-manylinux_2_31_aarch64.whl -O home_assistant_chip_core-2023.1.0-cp37-abi3-manylinux_2_31_aarch64.whl
+python3.11 -m pip install home_assistant_chip_core-2023.1.0-cp37-abi3-manylinux_2_31_aarch64.whl -t lib  --prefix ""
+
+wget -c https://github.com/home-assistant-libs/chip-wheels/releases/download/2023.1.0/home_assistant_chip_repl-2023.1.0-py3-none-any.whl -O home_assistant_chip_repl-2023.1.0-py3-none-any.whl
+python3.11 -m pip install home_assistant_chip_repl-2023.1.0-py3-none-any.whl -t lib  --prefix ""
+
+wget -c https://github.com/home-assistant-libs/chip-wheels/releases/download/2023.1.0/home_assistant_chip_clusters-2023.1.0-py3-none-any.whl -O home_assistant_chip_clusters-2023.1.0-py3-none-any.whl
+python3.11 -m pip install home_assistant_chip_clusters-2023.1.0-py3-none-any.whl -t lib  --prefix ""
+
+
+"$PIPPY" install -r requirements.txt -t lib --no-cache-dir --no-binary  :all: --prefix ""
+
 # Put package together
 cp -r lib pkg LICENSE manifest.json *.py README.md  css images js views  package/
 find package -type f -name '*.pyc' -delete

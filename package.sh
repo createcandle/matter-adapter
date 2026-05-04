f4d0ff193f5b2873aae2bb80a244f82b604d1d58
flatsiedatsie <unfold@gmail.com>
2026-05-04 22:50:03 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -233,7 +233,7 @@ echo ""
 #python3.11 -m pip install --upgrade pip
 #python3.11 -m pip install --upgrade setuptools wheel
 
-sudo apt install -y python3.11-distutils
+#sudo apt install -y python3.11-distutils
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
 python3.11 -m pip install --upgrade setuptools wheel
 
